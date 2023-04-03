9684a248bad1ac44a38bf7c3929c4a012b90a275
Luis Henrique Vieira Viana <luis.viana@asabranca.net>
2023-04-03 10:46:44 -0300

    Adição do filtro por variância

--- filters.py
+++ filters.py
@@ -20,3 +20,28 @@ def sum_filter(data, U, nClusters):
         V.append(aTotal)
     
     return V
+
+
+def variance_filter(data, U, nClusters):
+    V = []
+    nVar = data.shape[0]
+
+    for i in range(0, nVar):
+        aTotal = [] # Relevância total
+        nObj = data.shape[1]
+
+        for j in range(0, nClusters):  
+            a = 0
+            for k in range(0, nObj):
+                u = U[i][k][j] # Acessa o grau de pertinência do objeto k ao cluster j em relação a variável i
+                a += u
+        
+            a /= nObj # Relevância em relação ao cluster j
+            aTotal.append(a)
+
+        aTotal = np.asarray(aTotal)
+        variance = np.var(aTotal)
+
+        V.append(variance)
+    
+    return V

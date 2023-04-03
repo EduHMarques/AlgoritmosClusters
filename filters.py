c77d1c3d25a972116410d355766975bbd0f2032a
Luis Henrique Vieira Viana <luis.viana@asabranca.net>
2023-04-03 10:32:29 -0300

    Update na função sum_filter

--- filters.py
+++ filters.py
@@ -1,16 +1,22 @@
 import numpy as np
 
-def sum_filter(data, i, U, nClusters):
-    aTotal = 0 # Relevância total
-    nObj = data.shape[1]
+def sum_filter(data, U, nClusters):
+    V = []
+    nVar = data.shape[0]
 
-    for j in range(0, nClusters):  
-        a = 0
-        for k in range(0, nObj):
-            u = U[i][k][j] # Acessa o grau de pertinência do objeto k ao cluster j em relação a variável i
-            a += u
+    for i in range(0, nVar):
+        aTotal = 0 # Relevância total
+        nObj = data.shape[1]
+
+        for j in range(0, nClusters):  
+            a = 0
+            for k in range(0, nObj):
+                u = U[i][k][j] # Acessa o grau de pertinência do objeto k ao cluster j em relação a variável i
+                a += u
+        
+            a /= nObj # Relevância em relação ao cluster j
+            aTotal += a
+
+        V.append(aTotal)
     
-        a /= nObj # Relevância em relação ao cluster j
-        aTotal += a
-
-    return aTotal
+    return V
